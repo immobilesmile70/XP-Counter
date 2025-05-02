cca8c6dcf4ab1fe2a625d71468396ae301aef9c9
Shourya <immobilesmile70@users.noreply.github.com>
2025-05-02 23:12:19 +0530

    Added theme switcher

--- script.js
+++ script.js
@@ -697,33 +697,50 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 350);
     }
 
-    var x, i, j, l, ll, selElmnt, a, b, c;
+
+    function getSystemTheme() {
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    }
+
+    function applyTheme(theme) {
+        const final = theme === 'system' ? getSystemTheme() : theme;
+        document.documentElement.setAttribute("data-theme", final);
+    }
+
+    // On load
+    const savedTheme = localStorage.getItem("theme") || "system";
+    applyTheme(savedTheme);
+
+    // Custom select logic
+    var x, i, j, selElmnt, a, b, c;
     x = document.getElementsByClassName("custom-select");
-    l = x.length;
-    for (i = 0; i < l; i++) {
+    for (i = 0; i < x.length; i++) {
         selElmnt = x[i].getElementsByTagName("select")[0];
-        ll = selElmnt.length;
         a = document.createElement("DIV");
         a.setAttribute("class", "select-selected");
         a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
         x[i].appendChild(a);
         b = document.createElement("DIV");
         b.setAttribute("class", "select-items select-hide");
-        for (j = 1; j < ll; j++) {
+
+        for (j = 0; j < selElmnt.length; j++) {
             c = document.createElement("DIV");
             c.innerHTML = selElmnt.options[j].innerHTML;
             c.addEventListener("click", function (e) {
-                var y, i, k, s, h, sl, yl;
-                s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-                sl = s.length;
-                h = this.parentNode.previousSibling;
-                for (i = 0; i < sl; i++) {
-                    if (s.options[i].innerHTML == this.innerHTML) {
+                var s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+                var h = this.parentNode.previousSibling;
+                for (var i = 0; i < s.length; i++) {
+                    if (s.options[i].innerHTML === this.innerHTML) {
                         s.selectedIndex = i;
                         h.innerHTML = this.innerHTML;
-                        y = this.parentNode.getElementsByClassName("same-as-selected");
-                        yl = y.length;
-                        for (k = 0; k < yl; k++) {
+
+                        // 🔥 Theme switch logic
+                        const selectedTheme = s.options[i].value;
+                        applyTheme(selectedTheme);
+                        localStorage.setItem("theme", selectedTheme);
+
+                        const y = this.parentNode.getElementsByClassName("same-as-selected");
+                        for (var k = 0; k < y.length; k++) {
                             y[k].removeAttribute("class");
                         }
                         this.setAttribute("class", "same-as-selected");
@@ -734,6 +751,7 @@ document.addEventListener("DOMContentLoaded", () => {
             });
             b.appendChild(c);
         }
+
         x[i].appendChild(b);
         a.addEventListener("click", function (e) {
             e.stopPropagation();
@@ -742,24 +760,24 @@ document.addEventListener("DOMContentLoaded", () => {
             this.classList.toggle("select-arrow-active");
         });
     }
+
     function closeAllSelect(elmnt) {
         var x, y, i, xl, yl, arrNo = [];
         x = document.getElementsByClassName("select-items");
         y = document.getElementsByClassName("select-selected");
-        xl = x.length;
-        yl = y.length;
-        for (i = 0; i < yl; i++) {
+        for (i = 0; i < y.length; i++) {
             if (elmnt == y[i]) {
                 arrNo.push(i)
             } else {
                 y[i].classList.remove("select-arrow-active");
             }
         }
-        for (i = 0; i < xl; i++) {
-            if (arrNo.indexOf(i)) {
+        for (i = 0; i < x.length; i++) {
+            if (arrNo.indexOf(i) === -1) {
                 x[i].classList.add("select-hide");
             }
         }
     }
+
     document.addEventListener("click", closeAllSelect);
 });
